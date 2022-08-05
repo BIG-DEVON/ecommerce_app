6752cb9c23ea99536584ebf82227219da16b0e8b
BIG-DEVON <ezeajie14@gmail.com>
2022-08-06 00:33:14 +0100

    ..

--- src/EcommerceApi/index.js
+++ src/EcommerceApi/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user")
 
- mongoose.connect("mongodb+srv://Devon:Devon@cluster0.ql2dejm.mongodb.net/shop?retryWrites=true&w=majority"
- ).then(() => console.log("DBConnection Successful"))
+dotenv.config();
+
+ mongoose.connect(process.env.MONGO_URL
+ ).then(() => 
+ console.log("DB Connection Successful"))
  .catch((err) => {
      console.log(err)
   });
 
-app.listen(500, ()=>{
+  app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!");
 });
